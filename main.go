a3b4a862489fa43d680403978e1daadb930830a1
Haugen <tobiashaugen@gmail.com>
2024-02-13 20:16:46 +0100

    Layout and htmx example usage.

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/a-h/templ"
 )
 
-func main() {
-	component := Homepage("Buddy!")
+type FormData struct {
+	URL string
+}
 
-	http.Handle("/", templ.Handler(component))
+func main() {
+	app := Homepage("Ip Map")
+
+	http.Handle("/", templ.Handler(app))
 
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "API response")
+	})
+
+	http.HandleFunc("/handle-form", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			return
+		}
+
+		details := FormData{
+			URL: r.FormValue("url"),
+		}
+
+		fmt.Println(details.URL)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(details)
 	})
 
 	fmt.Println("Listening on :3000")
